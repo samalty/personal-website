453a97d9b49c210ab69ca44d2b1cd0f09fcd0e26
Sam Alty <sam_alty@outlook.com>
2020-11-20 15:24:46 +0000

    Updates

---
+++ precache-manifest.360f144dccdd6626b3c8f118cd86435b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6478af608fe9fba5a28",
+    "url": "/static/css/main.49cd4ac9.chunk.css"
+  },
+  {
+    "revision": "a6478af608fe9fba5a28",
+    "url": "/static/js/main.a6478af6.chunk.js"
+  },
+  {
+    "revision": "22b6a57a63e5b777af62",
+    "url": "/static/css/1.ccb51ddb.chunk.css"
+  },
+  {
+    "revision": "22b6a57a63e5b777af62",
+    "url": "/static/js/1.22b6a57a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "912bb3aa22ce23a6c43600a52c8bb519",
+    "url": "/static/media/image2.912bb3aa.JPG"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "9c1431e38a3e9d8b5bd6af8d7001aaf5",
+    "url": "/static/media/css3.9c1431e3.png"
+  },
+  {
+    "revision": "dff36bad692fa63eb3583a4c8983e3c1",
+    "url": "/static/media/javascript.dff36bad.png"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "09aeab847260a350c73e2e547dce6cd9",
+    "url": "/static/media/bootstrap.09aeab84.png"
+  },
+  {
+    "revision": "fd6984368ba75ec90b8f9fe0cae05566",
+    "url": "/static/media/python.fd698436.png"
+  },
+  {
+    "revision": "9b6d7217c8e9f5953f207c33b0c77645",
+    "url": "/static/media/django.9b6d7217.png"
+  },
+  {
+    "revision": "7e482d7f09beab709b9b882ded9891a6",
+    "url": "/static/media/flask.7e482d7f.png"
+  },
+  {
+    "revision": "cde43891991f76a5e7df17ac6d373aff",
+    "url": "/static/media/mysql.cde43891.png"
+  },
+  {
+    "revision": "c1d08dfcdbfea9aefeb00576d5d1bbfa",
+    "url": "/static/media/mongodb.c1d08dfc.png"
+  },
+  {
+    "revision": "5861abf4e97e814076c629464632857b",
+    "url": "/static/media/git.5861abf4.png"
+  },
+  {
+    "revision": "fbe6fe0d8d0664df5bb837db790f3353",
+    "url": "/static/media/project1.fbe6fe0d.JPG"
+  },
+  {
+    "revision": "a6c9d7c594be190fae6582313efac5fe",
+    "url": "/static/media/project2.a6c9d7c5.JPG"
+  },
+  {
+    "revision": "228717303a3a0b39ec08ca1696cbdea9",
+    "url": "/static/media/project3.22871730.JPG"
+  },
+  {
+    "revision": "d3b0f5e9f67c5c7c0bacc32437384fbb",
+    "url": "/static/media/project4.d3b0f5e9.JPG"
+  },
+  {
+    "revision": "dfcc4e036ae171fd2f6739f80529a85c",
+    "url": "/static/media/k2sm.dfcc4e03.JPG"
+  },
+  {
+    "revision": "74b27f5187c2f609cca8c9c223c3536e",
+    "url": "/static/media/acceler8_2.74b27f51.JPG"
+  },
+  {
+    "revision": "7801ed6af62bb45195b43641c37420e9",
+    "url": "/static/media/weatherapp2.7801ed6a.PNG"
+  },
+  {
+    "revision": "d8f76f3825e37f7aa6fed57d8ffd6182",
+    "url": "/static/media/calculator.d8f76f38.PNG"
+  },
+  {
+    "revision": "53a4422effac19e6afc9370e787ebaf6",
+    "url": "/static/media/colander3.53a4422e.PNG"
+  },
+  {
+    "revision": "b7d52a3fb77ba43dd26e2527f2013a9d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.60cbe89075004fa35429b07468060234.js"
+  "/precache-manifest.360f144dccdd6626b3c8f118cd86435b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
